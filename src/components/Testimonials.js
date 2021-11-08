a907e62f76f4b48d6fcb2567ec8b1c033dce5888
Kabalsky <Kabalsky@protonmail.com>
2021-11-08 22:27:54 +0100

    add some components , clear index html

---
+++ src/components/Testimonials.js
@@ -0,0 +1,99 @@
+function Testimonials () {
+    return (
+        <section id="our-testimonials" className="our-testimonial">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-6 offset-lg-3">
+                <div className="main-title text-center">
+                  <h2 className="color-white">Testimonials</h2>
+                  <p className="color-white">Here could be a nice sub title</p>
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-lg-6 offset-lg-3">
+                <div className="testimonial_grid_slider">
+                  <div className="item">
+                    <div className="testimonial_grid">
+                      <div className="thumb">
+                        <img src="images/testimonial/1.jpg" alt="1.jpg"></img>
+                      </div>
+                      <div className="details">
+                        <h4>Augusta Silva</h4>
+                        <p>Sales Manager</p>
+                        <p className="mt25">
+                          Aliquam dictum elit vitae mauris facilisis at dictum
+                          urna dignissim donec vel lectus vel felis.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="item">
+                    <div className="testimonial_grid">
+                      <div className="thumb">
+                        <img src="images/testimonial/1.jpg" alt="1.jpg"></img>
+                      </div>
+                      <div className="details">
+                        <h4>Augusta Silva</h4>
+                        <p>Sales Manager</p>
+                        <p className="mt25">
+                          Aliquam dictum elit vitae mauris facilisis at dictum
+                          urna dignissim donec vel lectus vel felis.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="item">
+                    <div className="testimonial_grid">
+                      <div className="thumb">
+                        <img src="images/testimonial/1.jpg" alt="1.jpg"></img>
+                      </div>
+                      <div className="details">
+                        <h4>Augusta Silva</h4>
+                        <p>Sales Manager</p>
+                        <p className="mt25">
+                          Aliquam dictum elit vitae mauris facilisis at dictum
+                          urna dignissim donec vel lectus vel felis.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="item">
+                    <div className="testimonial_grid">
+                      <div className="thumb">
+                        <img src="images/testimonial/1.jpg" alt="1.jpg"></img>
+                      </div>
+                      <div className="details">
+                        <h4>Augusta Silva</h4>
+                        <p>Sales Manager</p>
+                        <p className="mt25">
+                          Aliquam dictum elit vitae mauris facilisis at dictum
+                          urna dignissim donec vel lectus vel felis.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="item">
+                    <div className="testimonial_grid">
+                      <div className="thumb">
+                        <img src="images/testimonial/1.jpg" alt="1.jpg"></img>
+                      </div>
+                      <div className="details">
+                        <h4>Augusta Silva</h4>
+                        <p>Sales Manager</p>
+                        <p className="mt25">
+                          Aliquam dictum elit vitae mauris facilisis at dictum
+                          urna dignissim donec vel lectus vel felis.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+    )
+}
+
+export default Testimonials
