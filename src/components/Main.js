9a32649ffa03ca9179034d93067045792eb4bd23
Kabalsky <Kabalsky@protonmail.com>
2021-11-30 13:47:40 +0100

    clean errors and warnings

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,15 @@
 import useFetch from "../hooks/useFetch";
 import { Link } from "react-router-dom";
 import MainItem from "./MainItem";
-import CitySearch from "../components/CitySearch";
-import { useContext } from "react";
-import AuthContext from '../contexts/AuthContext';
 
+import { useContext } from "react";
+import AuthContext from "../contexts/AuthContext";
 
 //http://apifindhome.seyhanakifov.com/api/Home/Get
 function Main() {
   const token = useContext(AuthContext);
   console.log(token);
-  const url = 'https://localhost:44382/api/Home/Get'
+  const url = "https://localhost:44382/api/Home/Get";
   const [items, error, isLoaded] = useFetch(url, token);
   if (error) {
     return <div>Error: {error.message}</div>;
@@ -19,44 +18,45 @@ function Main() {
   } else {
     return (
       <>
-      
-      <section id="feature-property" className="feature-property bgc-f7">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-12">
-              <Link to="#feature-property">
-                <div className="mouse_scroll">
-                  <div className="icon">
-                    <h4>Scroll Down</h4>
-                    <p>to discover more</p>
+        <section id="feature-property" className="feature-property bgc-f7">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-12">
+                <Link to="#feature-property">
+                  <div className="mouse_scroll">
+                    <div className="icon">
+                      <h4>Scroll Down</h4>
+                      <p>to discover more</p>
+                    </div>
+                    <div className="thumb">
+                      <img
+                        src="images/resource/mouse.png"
+                        alt="mouse.png"
+                      ></img>
+                    </div>
                   </div>
-                  <div className="thumb">
-                    <img src="images/resource/mouse.png" alt="mouse.png"></img>
-                  </div>
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="container ovh">
+            <div className="row">
+              <div className="col-lg-6 offset-lg-3">
+                <div className="main-title text-center mb40">
+                  <h2>Featured Properties</h2>
+                  <p>Handpicked properties by our team.</p>
                 </div>
-              </Link>
-            </div>
-          </div>
-        </div>
-        <div className="container ovh">
-          <div className="row">
-            <div className="col-lg-6 offset-lg-3">
-              <div className="main-title text-center mb40">
-                <h2>Featured Properties</h2>
-                <p>Handpicked properties by our team.</p>
               </div>
-            </div>
-            <div className="col-lg-12">
-              <div className="feature_property_slider">
-                {items.map((item) => (
-                  <MainItem key={item.id} data={item}></MainItem>
-                ))}
+              <div className="col-lg-12">
+                <div className="feature_property_slider">
+                  {items.map((item) => (
+                    <MainItem key={item.id} data={item}></MainItem>
+                  ))}
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </section>
-     
+        </section>
       </>
     );
   }
